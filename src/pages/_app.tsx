7644218e34967a281b72ec4419060400e66de5d1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-05 11:11:27 +0200

    fix: filenames and delete meilisearch local files
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
-import Header from "../components/header";
+import Header from "@/components/Header";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
