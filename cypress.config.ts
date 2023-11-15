2288b5bc9ef1ef201274e43988620a59bb88ac67
Lee <leesheppard2404@gmail.com>
2023-11-15 10:40:37 +0100

    refactor: added .env and baseUrl to config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "cypress";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
 
 export default defineConfig({
+  env: { ...process.env },
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
